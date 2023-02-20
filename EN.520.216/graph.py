a0efab98f48cb79f9a312f665f899bb353e94d4d
lj <julienabdougonzales@gmail.com>
2023-02-20 00:19:27 -0500

    updated hw

---
+++ EN.520.216/graph.py
@@ -0,0 +1,16 @@
+import math
+import matplotlib.pyplot as plt
+x_list = range(250, 500)
+k_const = 8.6*(10**(-5)) #eV
+q = 1 #e
+def n_i(temperature):
+    return (5.2*(10**(15))*(temperature**(1.5))*math.exp(-1.1/(2*k_const*temperature)))
+
+
+NAND = 8*10**(16)*1.4*10**(17)
+const_part = (k_const/q)
+y_list = [const_part*T*math.log(NAND/(n_i(T)**2))/math.log(math.e) for T in x_list]
+
+plt.plot(x_list, y_list, 'ro')
+plt.show()
+print(f"{x_list[4]}, {y_list[4]}")
